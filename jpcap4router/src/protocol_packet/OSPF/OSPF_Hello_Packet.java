0903be29449f30c67f8811971f23fe0a9fdb67d1
cferZ <472770699@qq.com>
2016-04-11 21:32:05 +0800

    add ospf(lsa,dd,hello,lsr,lsa,lsack) packet class but not test

--- jpcap4router/src/protocol_packet/OSPF/OSPF_Hello_Packet.java
+++ jpcap4router/src/protocol_packet/OSPF/OSPF_Hello_Packet.java
@@ -4,6 +4,22 @@ import jpcap.packet.IPPacket;
 import protocol_packet.OSPF_packet;
 
 public class OSPF_Hello_Packet extends OSPF_packet {
+	/**
+	 * variables in OSPF HELLO packet
+	 */
+	
+	public byte[] net_mask=new byte[4];
+	public byte[] hello_interval=new byte[2];
+	public byte options=0;
+	public byte router_priority=0;
+	public byte[] router_dead_interval=new byte[4];
+	public byte[] designed_router=new byte[4];
+	public byte[] bk_designed_router=new byte[4];
+	public byte[] lls_data=new byte[12];
+	public byte[] active_neighbor;
+	public OSPF_Hello_Packet(IPPacket p) {
+		// TODO Auto-generated constructor stub	
+	}
 	
 	
 }
