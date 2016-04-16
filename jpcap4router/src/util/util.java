25d8e5279811e2f11b71dd2f8ae0326a413f3aaf
cferZ <472770699@qq.com>
2016-04-17 00:05:16 +0800

    add method to calculate standard ip checksum

--- jpcap4router/src/util/util.java
+++ jpcap4router/src/util/util.java
@@ -2,6 +2,19 @@ package util;
 
 public class util {
 	public byte[] getchecksum(byte[] buff){
+		boolean isNeedPad=false;
+		int result=0;
+		if(buff.length%2!=0){
+			isNeedPad=true;
+		}
+		for(int i=0;i<buff.length;i+=2){
+			if(i){
+				
+			}
+			
+		}
 		return null;
 	}
+	public int NoPaddingChecksum(byte[] buff){return 0;}
+	public int PaddingChecksum(byte[] buff){return 0;}
 }
